44f80ea66689b4e323c700ef2aee1ab0664f475f
KIMCOYG <kjhkjh9753@gmail.com>
2020-08-13 02:22:59 +0900

    Entity Class 완료

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,10 +1,14 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
     @Id
     @GeneratedValue
@@ -18,5 +22,4 @@ public class Member {
 
     @OneToMany(mappedBy = "member") //연관관계의 주인이 아닌 거울일 뿐
     private List<Order> orders = new ArrayList<>();
-
 }
