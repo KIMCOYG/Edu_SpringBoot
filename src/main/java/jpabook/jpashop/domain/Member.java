1521f774f75c3c451286bf5141e43936bc89884a
KIMCOYG <kjhkjh9753@gmail.com>
2020-08-12 20:12:16 +0900

    DB 설계

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //연관관계의 주인이 아닌 거울일 뿐
+    private List<Order> orders = new ArrayList<>();
+
+}
